2629ac60e609e9813bfdaa30bc9025881fc20265
lukedust <52528643+lukedust@users.noreply.github.com>
2019-09-16 15:29:04 +0200

    Add files via upload

---
+++ testjava.js
@@ -0,0 +1,9 @@
+var my_canvas = document.getElementById("canvas");
+var context = my_canvas.getContext("2d");
+
+context.fillStyle="green";
+context.fillRect(0, 0, 200, 300);
+context.fillStyle="white";
+context.fillRect(200, 0, 200, 300);
+context.fillStyle="organe";
+context.fillRect(400, 0, 200, 300);
